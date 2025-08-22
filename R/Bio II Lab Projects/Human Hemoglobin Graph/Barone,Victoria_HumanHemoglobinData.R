7b5c479b9cabb15a28b747874ea7900994dc38fe
Victoria Barone <victoria.barone525@gmail.com>
2025-08-21 22:19:16 -0500

    Add files via upload

---
+++ R/Bio II Lab Projects/Human Hemoglobin Graph/Barone,Victoria_HumanHemoglobinData.R
@@ -0,0 +1,25 @@
+###Human Hemoglobin Data 
+
+getwd()
+
+library(readr)
+Human_Hemoglobin <- read_csv("Human Hemoglobin.csv")
+View(Human_Hemoglobin)
+
+summary(Human_Hemoglobin)
+
+table(Human_Hemoglobin$Country, Human_Hemoglobin$Hemoglobin_level)
+
+counts<-table(Human_Hemoglobin$Country, Human_Hemoglobin$Hemoglobin_level)
+percents=100*prop.table(counts, 2)
+
+boxplot(
+  Hemoglobin_level~Country, 
+  data=Human_Hemoglobin, 
+  ylab="Hemoglobin Level", 
+  xlab="Country", 
+  main = "Barone, Victoria: Human Hemoglobin Levels by Country", 
+  col=c("lightgreen", "pink", "lightblue", "lightyellow")
+)
+
+
